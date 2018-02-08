5b3be7983b86e5bc2171c192c2cc93f8925055b3
Tal Rasha <talrasha007@gmail.com>
2018-02-08 21:57:30 +0800

    fix indent

--- contracts/Random.sol
+++ contracts/Random.sol
@@ -1,18 +1,18 @@
 pragma solidity ^0.4.0;
 
 contract Random {
-  uint256 public lastRand;
+    uint256 public lastRand;
 
-	function Random() public {
-		lastRand = uint256(block.blockhash(block.number - 1));
-	}
+    function Random() public {
+        lastRand = uint256(block.blockhash(block.number - 1));
+    }
 
-	function gen256() public returns (uint256) {
-		lastRand =
-			uint256(keccak256(
-				lastRand ^ uint256(block.blockhash(block.number - 1)) ^ now
-			));
+    function gen256() public returns (uint256) {
+        lastRand =
+            uint256(keccak256(
+                lastRand ^ uint256(block.blockhash(block.number - 1)) ^ now
+            ));
 
-		return lastRand;
-	}
+        return lastRand;
+    }
 }
